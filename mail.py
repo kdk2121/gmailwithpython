432a5079bd6aa579846e41a8fc113b87c42b48ca
halt20 <kdk2121@gmail.com>
2016-10-16 23:33:08 +0900

    initial commit

---
+++ mail.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.MIMEText import MIMEText
+
+def sendMail(id,pw,to,subject,content):
+        HOST = 'smtp.gmail.com'
+        port = 587
+
+        msg = MIMEText(content, _charset='euc-kr')
+        msg['Subject'] = subject
+        msg['From'] = id
+        msg['To'] = to
+
+        s = smtplib.SMTP(HOST,port)
+	s.ehlo()
+	s.starttls()
+	s.ehlo()
+        s.login(id, pw)
+        s.sendmail(id, [to], msg.as_string())
+        s.quit
+
