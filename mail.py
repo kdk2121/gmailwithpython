8a9f6698f31fad9ac449c051d9c1d81359a90f44
halt20 <kdk2121@gmail.com>
2016-10-17 00:00:43 +0900

    class made

--- mail.py
+++ mail.py
@@ -1,20 +1,20 @@
 import smtplib
 from email.MIMEText import MIMEText
 
-def sendMail(id,pw,to,subject,content):
-        HOST = 'smtp.gmail.com'
-        port = 587
+class Mail:
 
-        msg = MIMEText(content, _charset='euc-kr')
-        msg['Subject'] = subject
-        msg['From'] = id
-        msg['To'] = to
+        def sendMail(self,id,pw,to,subject,content):
+                HOST = 'smtp.gmail.com'
+                port = 587
+                msg = MIMEText(content, _charset='euc-kr')
+                msg['Subject'] = subject
+                msg['From'] = id
+                msg['To'] = to
 
-        s = smtplib.SMTP(HOST,port)
-	s.ehlo()
-	s.starttls()
-	s.ehlo()
-        s.login(id, pw)
-        s.sendmail(id, [to], msg.as_string())
-        s.quit
-
+                s = smtplib.SMTP(HOST,port)
+                s.ehlo()
+                s.starttls()
+                s.ehlo()
+                s.login(id, pw)
+                s.sendmail(id, [to], msg.as_string())
+                s.quit
